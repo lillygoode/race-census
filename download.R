26f74e67eab276b658ac41c3036f516a2fbf935b
lillygoode <l.goode30@ncf.edu>
2026-09-09 14:31:34 +0000

    commit

--- download.R
+++ download.R
@@ -1,11 +1,40 @@
 library(tidyverse)
-library(primer.data)
+library(tidycensus)
 
-# Filter 2020 Decennial Census data for Sarasota County, FL
-sarasota_race <- census |>
-  filter(str_detect(name, "Sarasota"))
+if (!dir.exists("data")) {
+  dir.create("data", recursive = TRUE)
+}
 
-# Save data
+api_key <- Sys.getenv("CENSUS_API_KEY")
+
+if (api_key != "") {
+  census_api_key(api_key, install = FALSE)
+
+  sarasota_race <- get_decennial(
+    geography = "county",
+    state = "12",
+    county = "115",
+    variables = c(
+      total_pop = "P2_001N",
+      white = "P2_005N",
+      black = "P2_006N",
+      native_american = "P2_007N",
+      asian = "P2_008N",
+      pacific_islander = "P2_009N",
+      hispanic_latino = "P2_002N"
+    ),
+    year = 2020,
+    output = "wide"
+  ) |>
+    select(name, total_pop, white, black, native_american, asian, pacific_islander, hispanic_latino)
+} else {
+  if (!file.exists("data/sarasota_race.csv")) {
+    stop("CENSUS_API_KEY is not set, and no existing data/sarasota_race.csv file was found.")
+  }
+
+  sarasota_race <- read_csv("data/sarasota_race.csv")
+}
+
 write_rds(sarasota_race, "data/sarasota_race.rds")
 write_csv(sarasota_race, "data/sarasota_race.csv")
 
